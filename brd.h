fb5b9450e34c939699c982e6476ae935579dab86
Eduardo Costa <m4c0@users.noreply.github.com>
2026-06-15 19:36:50 +0100

    brd: avoid unsolvable puzzles

--- brd.h
+++ brd.h
@@ -11,6 +11,8 @@ static void brd_init(int w) {
   brd_w2 = w * w;
 
   for (int i = 0; i < brd_w2 - 1; i++) brd[i] = i + 1;
+  brd[brd_w2 - 1] = 0;
+
   for (int i = 0; i < brd_w2; i++) {
     for (int j = 0; j < brd_w2; j++) {
       if (rand() % 2) continue;
@@ -19,6 +21,34 @@ static void brd_init(int w) {
       brd[j] = tmp;
     }
   }
+
+  // https://www.lukelavalva.com/theoryofsliding
+  // The number of cycles and the manhattan distance of the empty slot can't
+  // have the same parity. Otherwise it is unsolvable.
+
+  int zero = 0;
+  int cycles = 0;
+  int cycled[100] = {0};
+  for (int i = 0; i < brd_w2; i++) {
+    if (!brd[i]) zero = i;
+    if (cycled[i]) continue;
+    cycles++;
+
+    int start = i;
+    int j = i;
+    do {
+      cycled[j] = 1;
+      j = brd[j];
+    } while (j != start);
+  }
+
+  int zx = brd_w - 1 - (zero % brd_w);
+  int zy = brd_w - 1 - (zero / brd_w);
+  int mht = zx + zy;
+  if ((mht % 2) != (cycles % 2)) return;
+
+  // Try again
+  brd_init(w);
 }
 
 static int brd_swap(int a, int b) {
