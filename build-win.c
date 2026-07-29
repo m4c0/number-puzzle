6e12f9ead0081f9c8c6a6e0c99f89a0d8bcc723d
Eduardo Costa <m4c0@users.noreply.github.com>
2026-07-29 22:57:53 +0100

    windows: slurp

--- build-win.c
+++ build-win.c
@@ -13,6 +13,24 @@ static void usage() {
   fprintf(stderr, "just call 'build' without arguments\n");
 }
 
+static char * slurp(const char * file, unsigned * sz_ptr) {
+  FILE * f = fopen(file, "rb");
+  assert(f);
+
+  assert(0 == fseek(f, 0, SEEK_END));
+  long sz = ftell(f);
+  assert(sz);
+  assert(0 == fseek(f, 0, SEEK_SET));
+
+  char * data = malloc(sz + 1);
+  assert(1 == fread(data, sz, 1, f));
+  data[sz] = 0;
+
+  fclose(f);
+  *sz_ptr = sz;
+  return data;
+}
+
 static int run(char ** args) {
   assert(args && args[0]);
 
