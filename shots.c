158ff85558762c79ed49aaf34c597947da26bc78
Eduardo Costa <m4c0@users.noreply.github.com>
2026-07-29 22:52:13 +0100

    shots: modernising

--- shots.c
+++ shots.c
@@ -2,13 +2,10 @@
 #include "stb_image_write.h"
 #include "vlk.h"
 
-#define W 2064
-#define H 2752
-
 unsigned vlk_open(const char * name, const char * ext, const void ** ptr) {
   char fname[1024];
 #ifdef _WIN32
-  sprintf(fname, "app/%s.%s", name, ext);
+  sprintf(fname, "%s.%s", name, ext);
 #else
   sprintf(fname, "puzzle.app/Contents/Resources/%s.%s", name, ext);
 #endif
@@ -38,12 +35,21 @@ HWND vlk_hwnd;
 void * vlk_metal_layer() { return NULL; }
 #endif
 
-int main() {
+void shot(unsigned w, unsigned h) {
+  char fn[1024];
+  snprintf(fn, 1024, "shot-%dx%d.png", w, h);
+
   vlk_init(0);
-
-  void * buf = vlk_headless(W, H);
-
+  void * buf = vlk_headless(w, h);
+  stbi_write_png(fn, w, h, 4, buf, w * 4);
   vlk_deinit();
+}
 
-  stbi_write_png("shot.png", W, H, 4, buf, W * 4);
+int main() {
+  // apple store
+  shot(1260, 2736);
+  shot(1284, 2778);
+ 
+  // itch
+  shot(630, 500);
 }
